04bb9ea45fac4cd92484bb664e9b8471dd64a370
木村輝 <hikarudjent@gmail.com>
2025-05-01 22:39:51 +0900

    プロフィール

--- lib/postDataFetcher.ts
+++ lib/postDataFetcher.ts
@@ -1,37 +1,67 @@
 import { prisma } from "./prisma";
 
-export async function fetchPosts(userId: string) {
-  const following = await prisma.follow.findMany({
-    where: {
-      followerId: userId,
-    },
-    select: {
-      followingId: true,
-    },
-  });
-  const followingIds = following.map((f) => f.followingId);
-  const posts = await prisma.post.findMany({
-    where: {
-      authorId: {
-        in: [userId, ...followingIds],
-      },
-    },
-    include: {
-      author: true,
-      likes: {
-        select: {
-          userId: true,
+export async function fetchPosts(userId: string, username?: string) {
+  if (username) {
+    // プロフィール
+    return await prisma.post.findMany({
+      where: {
+        author: {
+          username,
         },
       },
-      _count: {
-        select: {
-          replies: true,
+      include: {
+        author: true,
+        likes: {
+          select: {
+            userId: true,
+          },
+        },
+        _count: {
+          select: {
+            replies: true,
+          },
         },
       },
-    },
-    orderBy: {
-      createdAt: "desc",
-    },
-  });
-  return posts;
+      orderBy: {
+        createdAt: "desc",
+      },
+    });
+  }
+
+  if (!username) {
+    // タイムライン
+    const following = await prisma.follow.findMany({
+      where: {
+        followerId: userId,
+      },
+      select: {
+        followingId: true,
+      },
+    });
+    const followingIds = following.map((f) => f.followingId);
+    const posts = await prisma.post.findMany({
+      where: {
+        authorId: {
+          in: [userId, ...followingIds],
+        },
+      },
+      include: {
+        author: true,
+        likes: {
+          select: {
+            userId: true,
+          },
+        },
+        _count: {
+          select: {
+            replies: true,
+          },
+        },
+      },
+      orderBy: {
+        createdAt: "desc",
+      },
+    });
+    return posts;
+  }
 }

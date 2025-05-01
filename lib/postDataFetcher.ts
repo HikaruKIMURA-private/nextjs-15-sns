4041b6d0ae0acad2ba23560946afe534602b8013
木村輝 <hikarudjent@gmail.com>
2025-05-01 22:42:00 +0900

    型エラー

--- lib/postDataFetcher.ts
+++ lib/postDataFetcher.ts
@@ -28,40 +28,38 @@ export async function fetchPosts(userId: string, username?: string) {
     });
   }
 
-  if (!username) {
-    // タイムライン
-    const following = await prisma.follow.findMany({
-      where: {
-        followerId: userId,
+  // タイムライン
+  const following = await prisma.follow.findMany({
+    where: {
+      followerId: userId,
+    },
+    select: {
+      followingId: true,
+    },
+  });
+  const followingIds = following.map((f) => f.followingId);
+  const posts = await prisma.post.findMany({
+    where: {
+      authorId: {
+        in: [userId, ...followingIds],
       },
-      select: {
-        followingId: true,
-      },
-    });
-    const followingIds = following.map((f) => f.followingId);
-    const posts = await prisma.post.findMany({
-      where: {
-        authorId: {
-          in: [userId, ...followingIds],
+    },
+    include: {
+      author: true,
+      likes: {
+        select: {
+          userId: true,
         },
       },
-      include: {
-        author: true,
-        likes: {
-          select: {
-            userId: true,
-          },
-        },
-        _count: {
-          select: {
-            replies: true,
-          },
+      _count: {
+        select: {
+          replies: true,
         },
       },
-      orderBy: {
-        createdAt: "desc",
-      },
-    });
-    return posts;
-  }
+    },
+    orderBy: {
+      createdAt: "desc",
+    },
+  });
+  return posts;
 }
